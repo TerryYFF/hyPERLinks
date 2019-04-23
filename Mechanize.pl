c38cb9eb5fd4c2235e77b680233aed1f54b59173
Itaru <itaru.liverpool@gmail.com>
2019-04-22 22:22:24 -0400

    Yey

--- Mechanize.pl
+++
@@ -1,9 +0,0 @@
-use strict;
-use warnings;
-use WWW::Mechanize();
-
-my $mech = WWW::Mechanize->new()
-
-my $mech = WWW::Mechanize->new( agent => 'wonderbot 1.01' );
-
-$mech->agent_alias( 'Windows IE 6' );
--- hyPERLinks.pl
+++ hyPERLinks.pl
@@ -1,8 +1,5 @@
 use strict;
 use warnings;
-use LWP::Simple;
-use HTML::TreeBuilder;
-use HTML::FormatText;
 
 $|=1;
 my $word;
@@ -16,104 +13,23 @@ sub main {
   my $input = 'Input.txt';
   open(INPUT, $input) or die ("Input file $input not found.\n");
 
-  # my $check = 'Check.txt';
-  # open(CHECK, '>:encoding(UTF-8)', $check) or die ("Cannot create $check.\n");
-
 # Output file called Output.txt. This is where it will be written.
   my $output = 'Output.html';
   open(OUTPUT, '>:encoding(UTF-8)', $output) or die ("Cannot create $output.\n");
 
 
+my @lines;
 
+my $count = 0;
 
-# while (my $line = <INPUT>) {
-#     my $entire_file = split('\s', $line);
-#     # print OUTPUT "<a href=\"https://en.wikipedia.org/wiki/$1\"> $1 </a> \n";
-#     # print "size of array:".@entire_file.".\n";
-#     print OUTPUT "<a href=\"https://en.wikipedia.org/wiki/$entire_file\"> $entire_file </a> \n";
-#     # print $entire_file[0] . "\n";
-# }
-
-
-
-
-
-# my @lines;
-#
-# my $count = 0;
-#
-# while (my $line = <INPUT>) {
-#   chomp $line;
-#
-#   my @values = split /\s* \s*/, $line;
-#
-#   push @lines, \@values;
-#
-# }
-#
-# print $lines[0][1] . "\n";
-#
-# foreach my $line(@lines) {
-#   # print $line . "\n";
-# }
-
-
-
-
-
-
-# while (my $line =<INPUT>) { # While the INPUT has a line.
-# # if($line =~ /(\b\w*\s\b)/) { # if there is the word "is" then run it.
-#   if($line =~ /([,])/) { # if there is the word "is" then run it.
-#   # $line =~ s/Barton/Mike/ig; #changes the name Barton to Mike.
-#   # print "Matched: '$1 \n'";
-#     print OUTPUT "<a href=\"https://en.wikipedia.org/wiki/$1\"> $1 </a> \n";
-#   }
-# }
-
-
-# while (my $line = <INPUT>){
-#   print OUTPUT $line . "\n";
-#   if ($line =~ /()/)
-# }
-
-
-
-
-  #   while (<INPUT>) {
-  #     # split each input line; words are separated by whitespace
-  #     for $word (split) {
-  #       # print $word . "\n";
-  #       # i'm just printing each "word" on a new line.
-  #       print OUTPUT "<a href=\"https://en.wikipedia.org/wiki/$word\"> $word </a> \n";
-  #   }
-  # }
-
-
-
-
-# if (<INPUT> =~ /(\w+?\s+)/) {
-#   print "The shortest word is $1 \n";
-# }
-# if (<INPUT> =~ /(\w?\S+)/) {
-#   print "The Longest word is $1 \n";
-# }
-#
-# print <INPUT>;
-
-
-
-  # my $URL = get("https://en.wikipedia.org/wiki/Virginia");
-  # my $Format = HTML::FormatText->new;
-  # my $TreeBuilder = HTML::TreeBuilder->new;
-  # $TreeBuilder->parse($URL);
-  # # if ($TreeBuilder->parse($URL)) {
-  # #   print "Rip";
-  # # } else {
-  # #   print "Yes";
-  # # }
-  # my $Parsed = $Format->format($TreeBuilder);
-  # print CHECK $Parsed;
+while (my $line = <INPUT>) {
+  my @values = split /\s+/, $line;
+  foreach my $line(@values) {
+      print OUTPUT "<a href=\"https://en.wikipedia.org/wiki/$line\"> $line </a>";
+      # print OUTPUT $line . "\n";
+    }
+    print OUTPUT "<br>" . "\n";
+}
 
 
 
@@ -122,8 +38,6 @@ sub main {
 }
 
 main();
-
-
 
 
 # https://stackoverflow.com/questions/254345/how-can-i-extract-url-and-link-text-from-html-in-perl
